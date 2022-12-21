db61edf84d86061473e2fb9ba3ad0cee8fa31fd4
Anurag Kumar <119558896+Anurag12220795@users.noreply.github.com>
2022-12-21 13:34:46 +0530

    Create floop.py

---
+++ floop.py
@@ -0,0 +1,10 @@
+for i in range(1,10,2)
+#no. start 1 to 9 and different b/w 2
+#like 1,3,5,7,9
+print(i)
+
+for i in range(10,1,-2)
+#no. start 10 to 2  and different b/w -2
+#like 9,7,5,3,1
+print(i)
+
