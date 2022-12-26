c5ed8476dfc65bc63945d294cbe0db1f3f790af4
Anurag Kumar <119558896+Anurag12220795@users.noreply.github.com>
2022-12-26 20:10:51 +0530

    Add files via upload

---
+++ revering number.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> name="lovely professional university"
+>>> len1=len
+-
+>>> -
+SyntaxError: invalid syntax
+>>> 
++
+>>> 
+>>> name="lovely professional university"
+>>> len1=len(name)
+>>> for i in range (len1):
+...     print(name[len1-1-i])
